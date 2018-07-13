87d2ab05c397c04acfa59e7ec416070bcbe7a988
Tahsin-Mayeesha <tasmiah.tahsin@northsouth.edu>
2018-07-13 19:53:01 +0600

    Add unit test skeleton code

---
+++ mediaviz/tests/test_scaling.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+def test_get_distance():
+    pass
+
+    
+
+def test_get_pairwise_distance_between_largest_nodes():
+    pass
+
+
+def test_direct_scaling_ratio():
+   pass
+    
+
+def test_scale_layout():
+   pass
---
+++ mediaviz/tests/test_utils.py
@@ -0,0 +1,31 @@
+import pytest
+
+def test_set_node_color():
+    pass
+
+
+def test_set_node_size():
+    pass       
+
+
+def test_filter_graph():
+    pass
+
+def test_set_node_label():
+    pass
+
+def test_edgecolor_by_source():
+   pass
+
+        
+def test_get_subgraph_pos():
+   pass
+
+def test_rotate():
+    pass
+
+def test_rotation_layout():
+    pass
+
+def test_draw_networkx_nodes_custom():
+    pass
