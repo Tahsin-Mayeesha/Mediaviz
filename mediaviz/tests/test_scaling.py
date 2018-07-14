c3c2cb733349d01179ee24b07006c1980ff9ec71
Tahsin-Mayeesha <tasmiah.tahsin@northsouth.edu>
2018-07-14 22:34:41 +0600

    Update tests

--- mediaviz/tests/test_scaling.py
+++ mediaviz/tests/test_scaling.py
@@ -3,16 +3,21 @@ import pytest
 
 def test_get_distance():
     pass
-
+    # test with random point
     
 
 def test_get_pairwise_distance_between_largest_nodes():
     pass
-
+    # test for node size 0 
+    # test for null values
 
 def test_direct_scaling_ratio():
    pass
+   # check if it returns a null value or not
     
 
 def test_scale_layout():
    pass
+   # check if returned output pos dict is equal length to input
+   # check if all values has been scaled
+   # check for null values
--- mediaviz/tests/test_utils.py
+++ mediaviz/tests/test_utils.py
@@ -1,31 +1,50 @@
 import pytest
 
 def test_set_node_color():
+    # check for empty list
+    # check if the output list length is equal to number of nodes in graph
+    # check if there's any null values after setting the colormap
     pass
 
 
 def test_set_node_size():
+    # check for empty list
+    # check if the output list length is equal to the number of nodes in graph
+    # check if minimum and maxium values of the output is within the range given
     pass       
 
 
 def test_filter_graph():
+    # check if it returns a graph
+    # check if the number of graph nodes is equal to k
+    # check if error is raised when the graph is empty
+    # check if error is raised when K > number of nodes in the original graph
     pass
 
 def test_set_node_label():
+    
+    # check if the number of nodes in the input graph is equal to the dictionary length
+    # check if error is raised when attribute is missing for the node label
     pass
 
 def test_edgecolor_by_source():
-   pass
+    # check for empty list
+    # check if the number of items in the output color list is equal to number of edges in the graph
+    pass
+
 
         
 def test_get_subgraph_pos():
+    # check if the output has positions for all nodes in the subgraph
    pass
 
 def test_rotate():
     pass
-
+    # check with given point if the rotation is successful
 def test_rotation_layout():
     pass
+    # check for empty list
+    # check if the number of nodes in the rotated pos dict output is equal to number of input nodes
 
 def test_draw_networkx_nodes_custom():
     pass
